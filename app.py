d7bde95e8c02c4b1fca6de2406c20b37faa56d12
Zeffut <tom77ds@gmail.com>
2025-05-10 20:57:18 +0200

    Added Tracking Script

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(layout="wide")
+
+# Ajout du script de tracking
+st.markdown("""
+<script
+    src="https://srv814883.hstgr.cloud/api/script.js"
+    data-site-id="1"
+    defer
+></script>
+""", unsafe_allow_html=True)
+
 nav = get_nav_from_toml("pages.toml")
 pg = st.navigation(nav)
 add_page_title(pg)
