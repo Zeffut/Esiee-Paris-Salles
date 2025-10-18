98799aae4e2264f3e790eaf792e9c04c3e02c31d
Zeffut <74143023+Zeffut@users.noreply.github.com>
2025-10-18 17:31:44 +0200

    C

--- frontend/analytics.js
+++
@@ -1,9 +0,0 @@
-// Vercel Analytics & Speed Insights
-import { inject as injectAnalytics } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-// Initialize Analytics
-injectAnalytics();
-
-// Initialize Speed Insights
-injectSpeedInsights();
