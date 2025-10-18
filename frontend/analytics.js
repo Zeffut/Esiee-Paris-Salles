869e1f92354e11188712b39ad44eac21025ce5bc
Zeffut <74143023+Zeffut@users.noreply.github.com>
2025-10-18 17:29:43 +0200

    Added Vercel Tracking Tools

---
+++ frontend/analytics.js
@@ -0,0 +1,9 @@
+// Vercel Analytics & Speed Insights
+import { inject as injectAnalytics } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+// Initialize Analytics
+injectAnalytics();
+
+// Initialize Speed Insights
+injectSpeedInsights();
